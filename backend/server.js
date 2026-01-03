2fd0357b26af49f270be258f3e090afe74726311
rias-ak <ashwinkv05@gmail.com>
2026-01-03 16:13:50 +0530

    react app change

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "https://contact-manager.netlify.app" || "http://localhost:5173/",
+  origin: process.env.REACT_APP_API_URL || "http://localhost:5173/",
   methods: ["GET", "POST", "DELETE"],
   credentials: true
 }));
